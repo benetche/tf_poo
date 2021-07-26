49d4c8bcd693792a98ddeec6173c1584d9300cdd
VictorLutes <61762690+VictorLutes@users.noreply.github.com>
2021-07-25 22:12:24 -0300

    Update Comorbidades.java

--- src/Comorbidades.java
+++ src/Comorbidades.java
@@ -6,11 +6,11 @@ public class Comorbidades {
 	
 	@Override
 	public String toString() {
-		String possoVacinar="A lista de comorbidades aceitas no Brasil são:\n\t";
-		possoVacinar+="Diabetis Mellitus, Pneumopatias Crônicas Graves, Hipertensão Arterial Resistente, Hipertensão Arterial, Insuficiência Cardíaca, Cor-Pulmonale, Hipertensão Pulmonar";
+		String possoVacinar="A lista de comorbidades aceitas no Brasil são: <p align=\"justify\">";
+		possoVacinar+="&emsp Diabetis Mellitus, Pneumopatias Crônicas Graves, Hipertensão Arterial Resistente, Hipertensão Arterial, Insuficiência Cardíaca, Cor-Pulmonale, Hipertensão Pulmonar";
 		possoVacinar+=", Cardiopatia Hipertensiva, Síndromes Coronarianas, Valvopatias, Miocardiopatias, Pericardiopatiasm Doenças da aorta, Doença dos grandes vasos, Fístulas Arteriovenosas";
 		possoVacinar+=", Arritmias Cardíacas, Cardiopatias congênita no adulto, Próteses valvares, Dispositivos cardíacos implantados, doença renal crônica, Imunossuprimidos, Anemia falciforme";
-		possoVacinar+=", Obesidade mórbida, Síndorme de down e Cirrose hepática.";
+		possoVacinar+=", Obesidade mórbida, Síndorme de down e Cirrose hepática.</p>";
 		return possoVacinar;
 	}
 }
