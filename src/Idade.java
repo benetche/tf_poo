fb5ca43c6f0bb3c10f021cdb6497c3b8e6350cac
VictorLutes <61762690+VictorLutes@users.noreply.github.com>
2021-07-26 21:53:42 -0300

    Update Idade.java

--- src/Idade.java
+++ src/Idade.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.LocalDate;
 
 public class Idade {
